f8d1e827e66095e9769efba400b63df68ea7f429
Ola Popp <olapopp85@gmail.com>
2017-03-04 10:44:31 +0100

    test done

--- PointManagerTests/Models/CameraModelTest.cs
+++ PointManagerTests/Models/CameraModelTest.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PointManager.Models;
-using PointManager.Models.Tests;
 using PointManager.ViewModels.UNIT;
 using PointManagerTests.LegacyBehavior;
 
@@ -32,7 +26,7 @@ namespace PointManagerTests.Models
             
             _confirmedLogic = new MyCameraLogicKey { degH = 0, degV = 0, X = 1, Y = 1, Z = 1 };
         }
-
+        [TestMethod]
         public void X_PropTest()
         {
             //Arrange
@@ -40,7 +34,92 @@ namespace PointManagerTests.Models
             //Act
             _data.X = value;
             _confirmedLogic.X = value;
-            //
+            //Assert
+            Assert.IsTrue(_data.X==_confirmedLogic.X);
         }
+
+        [TestMethod]
+        public void Y_PropTest()
+        {
+            //Arrange
+            var value = 5;
+            //Act
+            _data.Y = value;
+            _confirmedLogic.Y = value;
+            //Assert
+            Assert.IsTrue(_data.Y == _confirmedLogic.Y);
+        }
+
+        [TestMethod]
+        public void Z_PropTest()
+        {
+            //Arrange
+            var value = 5;
+            //Act
+            _data.Z = value;
+            _confirmedLogic.X = value;
+            //Assert
+            Assert.IsTrue(_data.Z == _confirmedLogic.Z);
+        }
+        [TestMethod]
+        public void DegV_PropTest()
+        {
+            //Arrange
+            var value = 5;
+            //Act
+            _data.degV = value;
+            _confirmedLogic.degV = value;
+            //Assert
+            Assert.IsTrue(_data.degV == _confirmedLogic.degV);
+        }
+
+        [TestMethod]
+        public void DegH_PropTest()
+        {
+            //Arrange
+            var value = 5;
+            //Act
+            _data.degH = value;
+            _confirmedLogic.degH = value;
+            //Assert
+            Assert.IsTrue(_data.degH == _confirmedLogic.degH);
+        }
+
+        [TestMethod]
+        public void DegH_DegreesAbove360ResetTest()
+        {
+            //Arrange
+            var value = 365;
+            //Act
+            _data.degH = value;
+            _confirmedLogic.degH = value;
+            //Assert
+            Assert.IsTrue(_data.degH == _confirmedLogic.degH);
+        }
+
+        [TestMethod]
+        public void DegV_DegreesAbove360ResetTest()
+        {
+            //Arrange
+            var value = 365;
+            //Act
+            _data.degV = value;
+            _confirmedLogic.degV = value;
+            //Assert
+            Assert.IsTrue(_data.degV == _confirmedLogic.degV);
+        }
+
+        [TestMethod]
+        public void DegH_DegreesBelow0ResetTest()
+        {
+            //Arrange
+            var value = -1;
+            //Act
+            _data.degH = value;
+            _confirmedLogic.degH = value;
+            //Assert
+            Assert.IsTrue(_data.degH == _confirmedLogic.degH);
+        }
+
     }
 }
